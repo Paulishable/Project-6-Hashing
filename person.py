2bdcdb54d5a0320928fb1ae0e78700ed97371185
Paulishable <paulzelenski@msn.com>
2022-06-15 07:08:30 -0700

    added python dictionary hashing

--- person.py
+++
@@ -1,8 +0,0 @@
-class person():
-    def __int__(self):
-        in_left = 0.0
-        in_right = 0.0
-        my_weight = 200.0
-        out_left = (my_weight + in_left + in_right) / 2
-        out_right = out_left
-        my_coords = (0, 0)
--- pyramid.py
+++ pyramid.py
@@ -1,8 +1,60 @@
 from time import perf_counter
+import sys
 
 PERSON_WEIGHT = 200.0
-PYRAMID_DEPTH = 23
+PYRAMID_DEPTH = 7
+global function_calls, cache_hits
 function_calls = 0
+cache_hits = 0
+
+global dictionary
+dictionary = {}
+for i in range(0, PYRAMID_DEPTH):
+    for j in range(0, PYRAMID_DEPTH + 1):
+        dictionary[(i, j)] = -1.0
+
+
+def print_dictionary():
+    for i in range(0, PYRAMID_DEPTH):
+        for j in range(0, PYRAMID_DEPTH + 1):
+            print("i,j = ", i, j, dictionary[(i, j)])
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 1:
+        PYRAMID_DEPTH = int(sys.argv[1])
+
+
+def weight_on_dict(row, column):
+    """calculate the weight on each person in the pyramid"""
+    global function_calls, dictionary, cache_hits
+    function_calls += 1
+
+    if dictionary[(row, column)] != -1.0:
+        cache_hits += 1
+        return dictionary[(row, column)]
+
+    # case 1 top person
+    if row == 0:
+        dictionary[(row, column)] = 0.0
+        return 0.0
+
+    # case 2 person on left edge
+    if column == 0:
+        weight = (PERSON_WEIGHT + weight_on_dict(row - 1, column)) / 2
+        dictionary[(row, column)] = weight
+        return weight
+
+    # case 3 person on  right edge
+    if column == row:
+        weight = (PERSON_WEIGHT + weight_on_dict(row - 1, column - 1)) / 2
+        dictionary[(row, column)] = weight
+        return weight
+
+    # case 4 internal person  --- everyone else is calculated here
+    weight = (PERSON_WEIGHT + weight_on_dict(row - 1, column)) / 2 + (PERSON_WEIGHT + weight_on_dict(row - 1, column - 1)) / 2
+    dictionary[(row, column)] = weight
+    return weight
 
 
 def weight_on(row, column):
@@ -11,7 +63,8 @@ def weight_on(row, column):
     function_calls += 1
     # case 1 top person
     if row == 0:
-        return 0.0
+        weight = 0.0
+        return weight
 
     # case 2 person on left edge
     if column == 0:
@@ -25,19 +78,86 @@ def weight_on(row, column):
 
     # case 4 internal person  --- everyone else is calculated here
     weight = (PERSON_WEIGHT + weight_on(row - 1, column)) / 2 + (PERSON_WEIGHT + weight_on(row - 1, column - 1)) / 2
-
     return weight
 
 
+def write_the_weight_calculations_to_a_file():
+    global function_calls
+    function_calls = 0
+
+    print("For a pyramid of height = 7:", end=" ")
+    for i in range(1, PYRAMID_DEPTH):
+        print()
+        for j in range(0, i + 1):
+            print(f" %.2f" % (weight_on(i, j)), end=" ")
+
+
+def run_the_weight_calculations():
+    global function_calls, cache_hits
+    function_calls = cache_hits = 0
+    for i in range(0, PYRAMID_DEPTH):
+        for j in range(0, i + 1):
+            if i == 0:
+                pass
+            if i == j:
+                weight_on(i, j)
+            if i != 0 and i != j:
+                weight_on(i, j)
+
+
+def run_the_weight_calculations_with_dictionary():
+    global function_calls, cache_hits, dictionary
+    function_calls = cache_hits = 0
+
+    for i in range(0, PYRAMID_DEPTH):
+        for j in range(0, i + 1):
+            if i == 0:
+                pass
+            if i == j:
+                weight_on_dict(i, j)
+            if i != 0 and i != j:
+                weight_on_dict(i, j)
+
+
+def print_the_weight_calculations():
+    global function_calls, dictionary, cache_hits
+    function_calls = cache_hits = 0
+
+    for i in range(0, PYRAMID_DEPTH):
+        print(" ")
+        for j in range(0, i + 1):
+            print(f" %6.2f" % (weight_on(i, j)), end=" ")
+
+
+def print_the_weight_calculations_with_dictionary():
+    global function_calls, dictionary, cache_hits
+    function_calls = cache_hits = 0
+
+    for i in range(0, PYRAMID_DEPTH):
+        print(" ")
+        for j in range(0, i + 1):
+            print(f" %6.2f" % (weight_on_dict(i, j)), end=" ")
+
+
+# print_the_weight_calculations()
 start = perf_counter()
-
-print("For a pyramid of height = 7:", end=" ")
-for i in range(0, PYRAMID_DEPTH):
-    print()
-    for j in range(0, i + 1):
-        print(f" %.2f" % (weight_on(i, j)), end=" ")
-
+run_the_weight_calculations()
 end = perf_counter()
 print()
 print("function calls = ", function_calls)
-print(f"\n Pyramid height = {PYRAMID_DEPTH}      = ", "%.6f" % (end - start), "seconds")
+print("cache hits = ", cache_hits)
+print(f"\n Time for Pyramid height of {PYRAMID_DEPTH}      = ", "%.6f" % (end - start), "seconds")
+
+start = perf_counter()
+run_the_weight_calculations_with_dictionary()
+end = perf_counter()
+# print_the_weight_calculations_with_dictionary()
+
+print()
+print("with Dictionary - function calls = ", function_calls)
+print("cache hits = ", cache_hits)
+print(f"\n with Dictionary - Time for Pyramid height of {PYRAMID_DEPTH}      = ", "%.6f" % (end - start), "seconds")
+
+print_the_weight_calculations()
+print_the_weight_calculations_with_dictionary()
+
